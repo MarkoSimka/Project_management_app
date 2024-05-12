234a648d82711b03de8226df8f905d76da8ab22a
MarkoSimka <markosimonoski8173@gmail.com>
2024-05-12 21:33:17 +0200

    [MS] Updated migrations for Task and Project

---
+++ database/migrations/2024_05_03_111609_update_created_at_and_updated_at_in_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('created_at')->time()->change();
+            $table->timestamp('updated_at')->time()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('created_at')->change();
+            $table->timestamp('updated_at')->change();
+        });
+    }
+};
